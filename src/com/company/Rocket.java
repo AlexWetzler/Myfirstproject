ebb6b769fe5521ad4d9ab9395218228847633b01
DataType <bertiewetz@gmail.com>
2020-06-18 17:50:41 -0500

    This is my code!

--- src/com/company/Rocket.java
+++ src/com/company/Rocket.java
@@ -3,24 +3,24 @@ package com.company;
 public class Rocket {
 
     public static void main(String[] args) {
-//
-//        System.out.println("Hello World!");
-//        System.out.println("I love Greenhill");
-//        System.out.println("    /\\");
-//        System.out.println("   /  \\");
-//        System.out.println("  /    \\");
-//        System.out.println(" +------+");
-//        System.out.println(" |      |");
-//        System.out.println(" |      |");
-//        System.out.println(" +------+");
-//        System.out.println(" |      |");
-//        System.out.println(" |      |");
-//        System.out.println(" +------+");
-//        System.out.println(" |      |");
-//        System.out.println(" |      |");
-//        System.out.println(" +------+");
-//        System.out.println("    /\\");
-//        System.out.println("   /  \\");
-//        System.out.println("  /    \\");
+
+        System.out.println("Hello World!");
+        System.out.println("I love Greenhill");
+        System.out.println("    /\\");
+        System.out.println("   /  \\");
+        System.out.println("  /    \\");
+        System.out.println(" +------+");
+        System.out.println(" |      |");
+        System.out.println(" |      |");
+        System.out.println(" +------+");
+        System.out.println(" |      |");
+        System.out.println(" |      |");
+        System.out.println(" +------+");
+        System.out.println(" |      |");
+        System.out.println(" |      |");
+        System.out.println(" +------+");
+        System.out.println("    /\\");
+        System.out.println("   /  \\");
+        System.out.println("  /    \\");
     }
 }
