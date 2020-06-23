3c99b4586af2d8b8049cdf2864ed944f26b64070
DataType <bertiewetz@gmail.com>
2020-06-22 20:52:02 -0500

    This is my code!

--- src/com/company/Kshapechallengev2.java
+++ src/com/company/Kshapechallengev2.java
@@ -1,3 +1,13 @@
+/* Alex Wetzler
+make a new char that equals 65.
+create a for loop that repeats 7 times
+    within that loop, create another one that prints letters that reduce by one each time
+create another for loop that repeats 7 times
+    within that loop, create yet another one that prints letters that reduce by one each time
+    the only difference with this one is that it goes the other way
+
+ */
+
 package com.company;
 
 public class Kshapechallengev2 {
@@ -5,13 +15,13 @@ public class Kshapechallengev2 {
 
         char letter = 65;
 
-        for (int Top = 7; Top > 0; Top--) {
+        for (int Top = 6; Top > 0; Top--) { //you need a for loop in a for loop
             for (int i = 0; i < Top; i++) {
                 System.out.print((char) (letter + i) + " ");
             }
             System.out.println();
         }
-        for (int Top = 0; Top < 7; Top++) {
+        for (int Top = 0; Top < 7; Top++) { //this part is very similar to the part above. the only difference is that it goes the other way
             for (int i = 0; i < Top; i++) {
                 System.out.print((char) (letter + i) + " ");
             }
